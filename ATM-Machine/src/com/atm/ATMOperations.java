20a649e282d7bce65a384b7513dbdf223b9f0834
DenisIvan10 <147132990+DenisIvan10@users.noreply.github.com>
2024-09-03 12:46:08 +0300

    Delete ATM-Machine directory

--- ATM-Machine/src/com/atm/ATMOperations.java
+++
@@ -1,8 +0,0 @@
-package com.atm;
-
-public interface ATMOperations {
-    public void viewBalance();
-    public void withdrawAmount(double withdraw);
-    public void depositAmount(double deposit);
-    public void viewStatement();
-}
